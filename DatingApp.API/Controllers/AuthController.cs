eeab822db111564b8691fc665cf0510b206d37b2
WEBEX 2020 <webex2020@hotmail.com>
2020-01-23 22:48:01 +0530

    End of section 5

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -74,6 +74,7 @@ namespace DatingApp.API.Controllers
             var token = tokenHandler.CreateToken(tokenDescriptor);
 
             return Ok(new { token = tokenHandler.WriteToken(token) });
+
         }
     }
 }
